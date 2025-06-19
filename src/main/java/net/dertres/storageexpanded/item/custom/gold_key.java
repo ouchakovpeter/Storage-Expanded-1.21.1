c3098b75cf48d978ce0d2a5fe9a6968029042162
Peter Ouchakov <ouchakovpeter@gmail.com>
2025-06-19 09:19:27 -0400

    testing

--- src/main/java/net/dertres/storageexpanded/item/custom/gold_key.java
+++ src/main/java/net/dertres/storageexpanded/item/custom/gold_key.java
@@ -17,29 +17,4 @@ public class gold_key extends Item {
         super(properties);
     }
 
-    private void ensureKeyId(ItemStack stack) {
-        CompoundTag tag = stack.getOrCreateTag();
-        if (!tag.hasUUID("KeyID")) {
-            tag.putUUID("KeyID", UUID.randomUUID());
-        }
-    }
-
-    @Override
-    public InteractionResult useOn(UseOnContext context){
-        Level level = context.getLevel();
-        Block clickedblock = level.getBlockState(context.getClickedPos()).getBlock();
-
-        return super.useOn(context);
-    }
-
-    @Override
-    public void onCraftedBy(ItemStack stack, Level world, Player player) {
-        ensureKeyId(stack);
-        super.onCraftedBy(stack, world, player);
-    }
-
-    public static UUID getKeyId(ItemStack stack) {
-        CompoundTag tag = stack.getTag();
-        return (tag != null && tag.hasUUID("KeyID")) ? tag.getUUID("KeyID") : null;
-    }
 }
