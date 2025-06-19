9e253fd703984f00263f82e0b5d1d1c96048a57e
Peter Ouchakov <ouchakovpeter@gmail.com>
2025-06-19 08:23:37 -0400

    Broken Door

--- src/main/java/net/dertres/storageexpanded/item/custom/gold_key.java
+++ src/main/java/net/dertres/storageexpanded/item/custom/gold_key.java
@@ -2,6 +2,7 @@ package net.dertres.storageexpanded.item.custom;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
@@ -10,12 +11,18 @@ import net.minecraft.world.level.block.Block;
 
 import java.util.Properties;
 
+//item constructor
 public class gold_key extends Item {
-    public gold_key(Properties properties){
+    public gold_key(Properties properties) {
         super(properties);
     }
 
-
+    private void ensureKeyId(ItemStack stack) {
+        CompoundTag tag = stack.getOrCreateTag();
+        if (!tag.hasUUID("KeyID")) {
+            tag.putUUID("KeyID", UUID.randomUUID());
+        }
+    }
 
     @Override
     public InteractionResult useOn(UseOnContext context){
@@ -23,5 +30,16 @@ public class gold_key extends Item {
         Block clickedblock = level.getBlockState(context.getClickedPos()).getBlock();
 
         return super.useOn(context);
+    }
+
+    @Override
+    public void onCraftedBy(ItemStack stack, Level world, Player player) {
+        ensureKeyId(stack);
+        super.onCraftedBy(stack, world, player);
+    }
+
+    public static UUID getKeyId(ItemStack stack) {
+        CompoundTag tag = stack.getTag();
+        return (tag != null && tag.hasUUID("KeyID")) ? tag.getUUID("KeyID") : null;
     }
 }
